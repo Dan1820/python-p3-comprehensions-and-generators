5c94617b0e924cbb8cfcf4f9fe83c50d9fe2f5d7
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-05-18 11:41:28 +0300

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # numericals= range(1,100)
+    num_list = range(1, 10)
+    return [n for n in num_list if n % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
